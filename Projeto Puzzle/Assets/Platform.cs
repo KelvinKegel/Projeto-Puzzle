42430197f2d97c7a96a4dc31d49ce1f2ffa18146
KelvinKegel <51788952+KelvinKegel@users.noreply.github.com>
2019-08-26 12:14:58 -0300

    Plat

--- Projeto Puzzle/Assets/Platform.cs
+++ Projeto Puzzle/Assets/Platform.cs
@@ -1,18 +1,88 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private enum MovementType
     {
-        
+        Circular,
+        Straight,
+        RotateAround
+    }
+
+    Player player;
+
+    [SerializeField]
+    private Transform rotationCenter;
+
+    private Vector3 rotationPosition;
+
+    [SerializeField]
+    private float rotationRadius = 2f, angularSpeed = 2f;
+
+    private float angle = 0f;
+
+    [SerializeField]
+    private MovementType movementType_;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rotationPosition = rotationCenter.position;
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        switch (movementType_)
+        {
+            case MovementType.RotateAround:
+                rotateAround();
+                break;
+
+            case MovementType.Circular:
+                circularMovement();
+                break;
+
+            default:
+                circularMovement();
+                break;
+        }
+    }
+
+    private void circularMovement()
+    {
+        float posX, posY = 0f;
+
+        posX = rotationPosition.x + Mathf.Cos(angle) * rotationRadius;
+        posY = rotationPosition.y + Mathf.Sin(angle) * rotationRadius;
+        transform.position = new Vector3(posX, posY, transform.position.z);
+        angle += Time.deltaTime * angularSpeed;
+
+        if (angle >= 360f)
+            angle = 0f;
+    }
+
+    private void rotateAround()
+    {
+        transform.RotateAround(new Vector3(0, 0, 0), Vector3.up, 20 * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("chabdcbs");
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            print("chabdcbs");
+            player.transform.parent = transform;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player.transform.parent = null;
+        }
     }
 }
